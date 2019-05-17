ad8e8e4d71d4c78143cdf0d574d6b88dd6e3b095
pavate <50588499+pavate@users.noreply.github.com>
2019-05-17 11:36:20 -0400

    Fixed where the user is stuck in the withdraw and deposit menu

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -9,10 +9,13 @@ import java.util.Scanner;
 public class ATM {
 
     public static void main(String[] args) {
+
         //idChoice is used as a loop initiator when the customer enters their ID
         boolean idChoice;
+
         //We will be using this variable to be testing if the ID is correct
         int id;
+
         //boolean exitMenu is used in the loop when we exit the menu
         boolean exitMenu;
 
@@ -22,6 +25,7 @@ public class ATM {
 
         //We create first account
         Account account1 = new Account("Vinayak", 101);
+
         /*The method deposit is used as the customer deposits the
         amount when they make a new account */
         account1.deposit(50.0);
@@ -66,13 +70,13 @@ public class ATM {
 
     //Menu method
     /**
-     * 
-     * @param acc This is a class parameter which uses the 
-     * Object account to be used in the menu method 
+     *
+     * @param acc This is a class parameter which uses the Object account to be
+     * used in the menu method
      */
     public void menu(Account acc) {
         Scanner scan = new Scanner(System.in);
-        int inOption=0;//Used as a loop element for menu
+        int inOption = 0;//Used as a loop element for menu
         int caseError; //Used as a loop element for try and catch
         //Start Menu loop
         do {
@@ -84,10 +88,11 @@ public class ATM {
             System.out.println("Please choose what you'd like to do:");
             int menuOption = scan.nextInt();
 
+            //We are using switch to explore our menu options
             switch (menuOption) {
                 case 1://Shows the Account Info
                     System.out.println("Customer: " + acc.getCustomerName());
-                    System.out.printf("Account Balance: $%.2f\n",acc.getBalance());
+                    System.out.printf("Account Balance: $%.2f\n", acc.getBalance());
                     System.out.printf("Monthly interest earned: $%.2f", acc.monthlyInterest());
                     System.out.println("\n");
                     inOption = 1;
@@ -101,11 +106,11 @@ public class ATM {
                             caseError = 1;
                         } catch (Throwable e) {
                             System.out.println(e.getMessage());
-                             System.out.println("\n");
+                            System.out.println("\n");
                             caseError = 0;
                         }
 
-                    } while (caseError == 0);
+                    } while (caseError == 1);
                     inOption = 1;
                     break;
                 case 3://To Deposit amout to accounts
@@ -115,13 +120,13 @@ public class ATM {
                         double depositAmount = scan.nextDouble();
                         try {
                             acc.deposit(depositAmount);
-                            caseError= 1;
+                            caseError = 1;
                         } catch (Throwable e) {
                             System.out.println(e.getMessage());
                             System.out.println("\n");
-                            caseError= 0;
+                            caseError = 0;
                         }
-                    } while (caseError == 0);
+                    } while (caseError == 1);
                     inOption = 1;
                     break;
                 case 4://To exit the menu
