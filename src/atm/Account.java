f5448d6a95f42fc82027e5640983176bd4d77090
pavate <50588499+pavate@users.noreply.github.com>
2019-05-21 09:47:18 -0400

    21st May submission day, final checking

--- src/atm/Account.java
+++ src/atm/Account.java
@@ -1,21 +1,22 @@
 package atm;
 
 /**
+ * This is the running part of The Account class
  *
  * @author Vinayak
  */
 public class Account {
-    
+
     //Defining the 4 field sets required to set a basic account up
     private String customerName;
     private final int id;
     private double balance;
     private double annualInterestRate;
-    
+
     //Default constructor with customer name and their ID number
     /**
-     * 
-     * @param customerName This is a constructor parameter for customer name 
+     *
+     * @param customerName This is a constructor parameter for customer name
      * @param id This is a constructor parameter for the id of the customer
      */
     public Account(String customerName, int id) {
@@ -23,6 +24,7 @@ public class Account {
         this.id = id;
     }
 //setter to set the name of the customer
+
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
@@ -52,33 +54,35 @@ public class Account {
         return annualInterestRate;
     }
 
-/**
- * //Withdraw method to deduct money from account
- * @param amount User input to withdraw specified amount
- * from balance
- */
+    /**
+     * //Withdraw method to deduct money from account
+     *
+     * @param amount User input to withdraw specified amount from balance
+     */
     public void withdraw(double amount) {
+
         /* If the customer tries to withdraw an amount
         which is greater than their account balance */
-        if (((balance - amount) < 0)||(balance==0)) {
+        if (((balance - amount) < 0) || (balance == 0)) {
             throw new IllegalArgumentException("Insufficient Funds to process request");
+
             /*customer cant withdraw a "0" amount will throw an exception to
         draw a diffrent amount */
-        } else if ((amount < 0)||(amount==0)) {
-
+        }
+        if ((amount < 0) || (amount == 0)) {
             throw new IllegalArgumentException("Please enter a vaild amount to withdraw");
-        } else if (amount > 0) {
-          balance = balance - amount;
 
         }
-
+        if (amount > 0) {
+            balance = balance - amount;
+        }
     }
 
-/**
- * 
- * @param amount User input to deposit amount to their
- * account
- */
+    /**
+     * Deposit method is to add money to the balance of the account
+     *
+     * @param amount User input to deposit amount to their account
+     */
     public void deposit(double amount) {
         /* Only a amount greater than "0" can be 
         deposited or else throws exception*/
@@ -87,19 +91,32 @@ public class Account {
             balance = amount + balance;
         } else {
             throw new IllegalArgumentException("Please enter a vaild amount to withdraw");
-
         }
     }
 
-/**
- * Method to calculate the monthly interest of the given annual
- * interest rate
- * @return returns the monthly interest rate for a specified account
- * while taking the account balance and the annual interest rate set to it 
- */
+    /**
+     * Method to calculate the monthly interest of the given annual interest
+     * rate
+     *
+     * @return returns the monthly interest rate for a specified account while
+     * passing a account object through it
+     */
     public double monthlyInterest() {
         double amount = balance * (annualInterestRate / 12 / 100);
         return amount;
+    }
+
+    /**
+     *
+     * @return This returns the String value of the name of the customer account
+     * balance and monthly interest of the user
+     */
+    @Override
+    public String toString() {
+        String format = "Name of the customer.: %s " +"\n"
+                + "Account Balance:%.2f" + "\n"
+                + "Monthly interest earned: %s" ;
+        return String.format(format, customerName, balance, monthlyInterest());
     }
 
 }
